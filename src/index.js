dc8e4845a4504deec24d3e3cdeeb09ab9d5c63e5
nikam3322 <nikam3322@gmail.com>
2023-08-05 15:06:42 +0200

    feat: change index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let k = Math.abs(n).toString();
+    let i = 0;
+    let result = '';
+    while (i < k.toString().length) {
+        result = k[i] + result;
+        i += 1;
+    }
+    return result;
 }
